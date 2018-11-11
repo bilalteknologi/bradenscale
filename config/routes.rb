4958594644dc60c65dd28e40c957fd12a48555b7
Diskominfo <diskominfo@MacBook-Pro.local>
2018-11-12 01:11:23 +0800

    new fix process

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,16 @@ Rails.application.routes.draw do
   # resources :questions
   # resources :answers
   devise_for :users, :skip => [:registrations, :recoverable]
+  scope "/admin" do
+    resources :users
+  end
   devise_scope :user do
     get "/login" => "devise/sessions#new", :as => :new_user_session_path
-    root to: "devise/sessions#new"
     post "/login" => "devise/sessions#create", :as => :user_session_path
     delete "/logout" => "devise/sessions#destroy", :as => :destroy_user_session_path
   end
+
+  root to: "transactions#index"
 
   resources :questions do
     resources :answers, only: [:create, :destroy, :edit, :update]
